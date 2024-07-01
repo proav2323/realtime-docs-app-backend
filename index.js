51644fca1b67466c514c5a870b71b76571a1d035
Ansh Verma <108835201+proav2323@users.noreply.github.com>
2024-07-01 17:58:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ const io = new Server(httpServer, {
 io.on("connection", (socket) => {
   socket.on("textChange", (data) => {
     socket.to(data.room).emit("recieveText", data.text);
+    console.log("done");
+    console.log(scoket.rooms);
   });
 
   socket.on("joinRoom", (room) => {
     socket.join(room);
+    console.log("room joined");
   });
 });
 
