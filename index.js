ece1d435131bcb3d146d9d4a7b446b8037f87d4b
proav2323 <108835201+proav2323@users.noreply.github.com>
2024-07-01 17:36:24 -0500

    coding

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 const httpServer = createServer(app);
 const io = new Server(httpServer, {});
 
